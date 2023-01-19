b43f9afbd58751394290b727ac9043bdf0f4cdf5
Eliot <nagyeliot@gmail.com>
2023-01-19 14:40:24 +0100

    Creating project

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+class EasyShopping(MDApp):
+    def build(self):
+        boy_layout = BoxLayout()
+
+
+if __name__ == '__main__':
+    EasyShopping().run()
