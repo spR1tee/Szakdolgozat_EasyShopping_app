600c6e41bcf49df970f1d5e6b6a36441402fefef
Eliot <nagyeliot@gmail.com>
2023-01-24 17:22:28 +0100

    Registration GUI + ScreenManagement

--- kivy_lang/kivy_lang.py
+++ kivy_lang/kivy_lang.py
@@ -1,21 +1,147 @@
 KV = """
-MDScreen:
-    md_bg_color: "white"
+WindowManager:
+    LoginScreen:
+    RegisterScreen:
+
+<LoginScreen>:
+    name: "login"
     
-    FitImage:
-        source: "img/logo.png"
-        size_hint: None, None
-        pos_hint: {'center_x': 0.5, 'center_y': 0.85}
-        width: "200dp"
-        height: "200dp"
+    MDCard:
+        orientation: "vertical"
+        size: root.width, root.height
+        spacing: 10
+        padding: 15
+        elevation: 10
+        md_bg_color: "white"
+    
+        FitImage:
+            source: "img/logo.png"
+            size_hint: None, None
+            pos_hint: {'center_x': 0.5, 'center_y': 0.9}
+            width: "200dp"
+            height: "200dp"
+        
+        MDBoxLayout:
+            orientation: "vertical"
+            spacing: 10
+            size_hint: None, None
+            pos_hint: {'center_x': 0.5, 'center_y': 0.50}
+            adaptive_height: True
+            adaptive_width: True
+            
+            MDTextField:
+                id: user_email
+                hint_text: "E-mail cím"
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                width: "300dp"
+                icon_left: "email"
+            
+            MDTextField:
+                id: user_password
+                hint_text: "Jelszó"
+                password: True
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                width: "300dp"
+                icon_left: "key-variant"
+                
+            MDBoxLayout:
+                orientation: "horizontal"
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                adaptive_height: True
+                adaptive_width: False
+                width: "300dp"
+                spacing: "48dp"
+                
+                MDFillRoundFlatIconButton:
+                    text: "Bejelentkezés"
+                    icon: "login"
+                    font_name: "fonts/Comfortaa-Regular.ttf"
+                    size_hint: None, None
+                    width: "300dp"
+                    on_press: app.login()
+                
+                MDTextButton:
+                    text: "Elfelejtett jelszó"
+                    underline: True
+                    font_name: "fonts/Comfortaa-Regular.ttf"
+                    font_size: 12
+                    size_hint: None, None
+                
+            MDLabel:
+                text: "Ha még nincs fiókod, itt regisztrálhatsz egyet:"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                adaptive_height: True
+                adaptive_width: False
+                width: "300dp"
+                font_size: 10
+                size_hint: None, None
+                halign: "center"
+            
+            MDFillRoundFlatIconButton:
+                text: "Regisztráció"
+                icon: "note"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                pos_hint: {'center_x': 0.5}
+                size_hint: None, None
+                width: "300dp"
+                on_press: 
+                    app.root.current = "register"
+                    root.manager.transition.direction = "right"
+            
+            MDLabel:
+                text: "vagy"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                adaptive_height: True
+                adaptive_width: False
+                width: "300dp"
+                font_size: 10
+                size_hint: None, None
+                halign: "center"
+            
+            MDFillRoundFlatIconButton:
+                text: "Folytatás vendégként"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                pos_hint: {'center_x': 0.5}
+                size_hint: None, None
+                width: "300dp"
+                on_press: app.join_as_guest()
+    
+<RegisterScreen>:
+    name: "register"
     
     MDBoxLayout:
         orientation: "vertical"
-        spacing: 12
+        spacing: 10
         size_hint: None, None
-        pos_hint: {'center_x': 0.5, 'center_y': 0.40}
+        pos_hint: {'center_x': 0.5}
         adaptive_height: True
         adaptive_width: True
+        
+        MDBoxLayout:
+            orientation: "horizontal"
+            spacing: 15
+            adaptive_height: True
+            adaptive_width: True
+        
+            MDIconButton:
+                icon: "arrow-left"
+                size_hint: None, None
+                pos_hint: {'center_x': 0.5}
+                on_press: 
+                    app.root.current = "login"
+                    root.manager.transition.direction = "left"
+            
+            MDLabel:
+                text: "Regisztráció"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                adaptive_width: True
+                font_size: 15
+        
+        Widget:
+            height: "100dp"
         
         MDTextField:
             id: user_email
@@ -28,45 +154,28 @@ MDScreen:
         MDTextField:
             id: user_password
             hint_text: "Jelszó"
-            password: True
             pos_hint: {'center_x': 0.5}
             size_hint_x: None
             width: "300dp"
             icon_left: "key-variant"
+            password: True
             
-        MDBoxLayout:
-            orientation: "horizontal"
+        MDTextField:
+            id: user_password_again
+            hint_text: "Jelszó újra"
             pos_hint: {'center_x': 0.5}
             size_hint_x: None
-            adaptive_height: True
-            adaptive_width: False
             width: "300dp"
-            spacing: "48dp"
-            
-            MDFillRoundFlatIconButton:
-                text: "Bejelentkezés"
-                icon: "login"
-                font_name: "fonts/Comfortaa-Regular.ttf"
-                size_hint: None, None
-                width: root.width*0.4
-                on_press: app.login()
-            
-            MDTextButton:
-                text: "Elfelejtett jelszó"
-                underline: True
-                font_name: "fonts/Comfortaa-Regular.ttf"
-                font_size: 12
-                size_hint: None, None
-            
-        MDLabel:
-            text: "Ha még nincs fiókod, itt regisztrálhatsz egyet:"
-            font_name: "fonts/Comfortaa-Regular.ttf"
-            adaptive_height: True
-            adaptive_width: False
+            icon_left: "key-variant"
+            password: True
+        
+        MDTextField:
+            id: date_of_birth
+            hint_text: "Születési dátum"
+            pos_hint: {'center_x': 0.5}
+            size_hint_x: None
             width: "300dp"
-            font_size: 10
-            size_hint: None, None
-            halign: "center"
+            icon_left: "calendar"
         
         MDFillRoundFlatIconButton:
             text: "Regisztráció"
@@ -74,22 +183,8 @@ MDScreen:
             font_name: "fonts/Comfortaa-Regular.ttf"
             pos_hint: {'center_x': 0.5}
             size_hint: None, None
-            width: root.width*0.4
-        
-        MDLabel:
-            text: "vagy"
-            font_name: "fonts/Comfortaa-Regular.ttf"
-            adaptive_height: True
-            adaptive_width: False
             width: "300dp"
-            font_size: 10
-            size_hint: None, None
-            halign: "center"
-        
-        MDFillRoundFlatIconButton:
-            text: "Folytatás vendégként"
-            font_name: "fonts/Comfortaa-Regular.ttf"
-            pos_hint: {'center_x': 0.5}
-            size_hint: None, None
-            width: root.width*0.4
+            on_press:
+                app.sign_up()
+                root.manager.transition.direction = "left"
 """
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFillRoundFlatButton
 from kivy_lang.kivy_lang import KV
 import pyrebase
 
@@ -18,8 +21,25 @@ auth = firebase.auth()
 db = firebase.database()
 
 
+class WindowManager(ScreenManager):
+    pass
+
+
+class LoginScreen(Screen):
+    pass
+
+
+class RegisterScreen(Screen):
+    pass
+
+
+class HomeScreen(Screen):
+    pass
+
+
 class EasyShopping(MDApp):
     # firebase_url = "https://easyshopping-8e66f-default-rtdb.europe-west1.firebasedatabase.app/.json"
+    dialog = None
     currently_logged_in_token = None
 
     def build(self):
@@ -28,21 +48,34 @@ class EasyShopping(MDApp):
         return Builder.load_string(KV)
 
     def sign_up(self):
+        if self.root.get_screen("register").ids.user_email.text is "" or self.root.get_screen(
+                "register").ids.user_password.text is "" or self.root.get_screen(
+            "register").ids.user_password_again.text is "":
+            self.open_error_dialog("Az összes mezőt kötelező kitölteni!")
+            return
+
+        if self.root.get_screen("register").ids.user_password.text != self.root.get_screen(
+                "register").ids.user_password_again.text:
+            self.open_error_dialog("A megadott jelszavak nem egyeznek!")
+            return
         try:
-            auth.create_user_with_email_and_password(self.root.ids.user_email.text, self.root.ids.user_password.text)
+            auth.create_user_with_email_and_password(self.root.get_screen("register").ids.user_email.text,
+                                                     self.root.get_screen("register").ids.user_password.text)
+            # TODO adatbázisba eltárolni a user adatokat
+            self.root.current = "login"
         except Exception:
-            print("E-Mail already used!")
-            # TODO
+            # TODO túl rövid jelszó vagy használt email probléma eldöntésének lekezelése
+            self.open_error_dialog("Már létezik ilyen Email cím!")
 
     def login(self):
         try:
-            login = auth.sign_in_with_email_and_password(self.root.ids.user_email.text,
-                                                         self.root.ids.user_password.text)
+            login = auth.sign_in_with_email_and_password(self.root.get_screen("login").ids.user_email.text,
+                                                         self.root.get_screen("login").ids.user_password.text)
             self.currently_logged_in_token = login["idToken"]
             self.currently_logged_in_token = auth.refresh(login["refreshToken"])
+            print("nice")
         except Exception:
-            print("Invalid E-Mail or Password")
-            # TODO
+            self.open_error_dialog("Nem megfelelő felhasználónév vagy jelszó!")
 
     def forgotten_password(self):
         pass
@@ -52,6 +85,15 @@ class EasyShopping(MDApp):
         login = auth.sign_in_anonymous()
         self.currently_logged_in_token = login["idToken"]
         self.currently_logged_in_token = auth.refresh(login["refreshToken"])
+
+    def open_error_dialog(self, error_text):
+        close_button = MDFillRoundFlatButton(text="Vissza", on_release=self.close_dialog)
+        self.dialog = MDDialog(title="Hiba", text=error_text,
+                               size_hint=(0.7, 1), buttons=[close_button])
+        self.dialog.open()
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()
 
 
 if __name__ == '__main__':
