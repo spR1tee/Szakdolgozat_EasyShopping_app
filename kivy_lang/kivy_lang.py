31d3dbedc4cc5000f8217345df8e38942c57da09
Eliot <nagyeliot@gmail.com>
2023-01-25 17:53:03 +0100

    Store data + GUI improvements

--- kivy_lang/kivy_lang.py
+++ kivy_lang/kivy_lang.py
@@ -2,6 +2,56 @@ KV = """
 WindowManager:
     LoginScreen:
     RegisterScreen:
+    HomeScreen:
+
+<HomeScreen>:
+    name: "home"
+
+<ClickableTextFieldRound>
+    size_hint_y: None
+    height: user_password.height
+    text: user_password.text
+    
+    MDTextField:
+        id: user_password
+        hint_text: root.hint_text
+        password: True
+        pos_hint: {'center_x': 0.5}
+        size_hint_x: None
+        width: "300dp"
+        icon_left: "key-variant"
+    
+    MDIconButton:
+        icon: "eye-off"
+        pos_hint: {"center_y": .5}
+        pos: user_password.width - self.width + dp(8), 0
+        theme_text_color: "Hint"
+        on_release:
+            self.icon = "eye" if self.icon == "eye-off" else "eye-off"
+            user_password.password = False if user_password.password is True else True
+
+<ClickableTextFieldRoundPasswordAgain>
+    size_hint_y: None
+    height: user_password_again.height
+    text: user_password_again.text
+    
+    MDTextField:
+        id: user_password_again
+        hint_text: root.hint_text
+        password: True
+        pos_hint: {'center_x': 0.5}
+        size_hint_x: None
+        width: "300dp"
+        icon_left: "key-variant"
+    
+    MDIconButton:
+        icon: "eye-off"
+        pos_hint: {"center_y": .5}
+        pos: user_password_again.width - self.width + dp(8), 0
+        theme_text_color: "Hint"
+        on_release:
+            self.icon = "eye" if self.icon == "eye-off" else "eye-off"
+            user_password_again.password = False if user_password_again.password is True else True
 
 <LoginScreen>:
     name: "login"
@@ -37,14 +87,12 @@ WindowManager:
                 width: "300dp"
                 icon_left: "email"
             
-            MDTextField:
-                id: user_password
-                hint_text: "Jelszó"
-                password: True
-                pos_hint: {'center_x': 0.5}
+            ClickableTextFieldRound:
+                id: login_pw
                 size_hint_x: None
+                hint_text: "Jelszó"
                 width: "300dp"
-                icon_left: "key-variant"
+                pos_hint: {"center_x": 0.5}
                 
             MDBoxLayout:
                 orientation: "horizontal"
@@ -87,7 +135,7 @@ WindowManager:
                 pos_hint: {'center_x': 0.5}
                 size_hint: None, None
                 width: "300dp"
-                on_press: 
+                on_press:
                     app.root.current = "register"
                     root.manager.transition.direction = "right"
             
@@ -112,79 +160,94 @@ WindowManager:
 <RegisterScreen>:
     name: "register"
     
-    MDBoxLayout:
+    MDCard:
         orientation: "vertical"
+        size: root.width, root.height
         spacing: 10
-        size_hint: None, None
-        pos_hint: {'center_x': 0.5}
-        adaptive_height: True
-        adaptive_width: True
-        
+        padding: 15
+        elevation: 10
+        md_bg_color: "white"
+    
         MDBoxLayout:
-            orientation: "horizontal"
-            spacing: 15
+            orientation: "vertical"
+            spacing: 10
+            size_hint: None, None
+            pos_hint: {'center_x': 0.5}
             adaptive_height: True
             adaptive_width: True
-        
-            MDIconButton:
-                icon: "arrow-left"
-                size_hint: None, None
-                pos_hint: {'center_x': 0.5}
-                on_press: 
-                    app.root.current = "login"
-                    root.manager.transition.direction = "left"
+            md_bg_color: "white"
             
-            MDLabel:
-                text: "Regisztráció"
-                font_name: "fonts/Comfortaa-Regular.ttf"
+            MDBoxLayout:
+                orientation: "horizontal"
+                spacing: 15
+                adaptive_height: True
                 adaptive_width: True
-                font_size: 15
-        
-        Widget:
-            height: "100dp"
-        
-        MDTextField:
-            id: user_email
-            hint_text: "E-mail cím"
-            pos_hint: {'center_x': 0.5}
-            size_hint_x: None
-            width: "300dp"
-            icon_left: "email"
-        
-        MDTextField:
-            id: user_password
-            hint_text: "Jelszó"
-            pos_hint: {'center_x': 0.5}
-            size_hint_x: None
-            width: "300dp"
-            icon_left: "key-variant"
-            password: True
             
-        MDTextField:
-            id: user_password_again
-            hint_text: "Jelszó újra"
-            pos_hint: {'center_x': 0.5}
-            size_hint_x: None
-            width: "300dp"
-            icon_left: "key-variant"
-            password: True
-        
-        MDTextField:
-            id: date_of_birth
-            hint_text: "Születési dátum"
-            pos_hint: {'center_x': 0.5}
-            size_hint_x: None
-            width: "300dp"
-            icon_left: "calendar"
-        
-        MDFillRoundFlatIconButton:
-            text: "Regisztráció"
-            icon: "note"
-            font_name: "fonts/Comfortaa-Regular.ttf"
-            pos_hint: {'center_x': 0.5}
-            size_hint: None, None
-            width: "300dp"
-            on_press:
-                app.sign_up()
-                root.manager.transition.direction = "left"
+                MDIconButton:
+                    icon: "arrow-left"
+                    size_hint: None, None
+                    pos_hint: {'center_x': 0.5}
+                    on_press: 
+                        app.root.current = "login"
+                        root.manager.transition.direction = "left"
+                
+                MDLabel:
+                    text: "Regisztráció"
+                    font_name: "fonts/Comfortaa-Regular.ttf"
+                    adaptive_width: True
+                    font_size: 15
+            
+            Widget:
+                height: "100dp"
+            
+            MDTextField:
+                id: user_email
+                hint_text: "E-mail cím"
+                helper_text: "Ezzel az E-mail címmel tudsz majd bejelentkezni"
+                helper_text_mode: "on_focus"
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                width: "300dp"
+                icon_left: "email"
+            
+            ClickableTextFieldRound:
+                id: register_pw
+                size_hint_x: None
+                hint_text: "Jelszó"
+                width: "300dp"
+                pos_hint: {"center_x": 0.5}
+                
+            ClickableTextFieldRoundPasswordAgain:
+                id: register_pw_again
+                size_hint_x: None
+                hint_text: "Jelszó újra"
+                width: "300dp"
+                pos_hint: {"center_x": 0.5}
+            
+            MDTextField:
+                id: date_of_birth
+                hint_text: "Születési dátum"
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                width: "300dp"
+                icon_left: "calendar"
+            
+            MDTextField:
+                id: username
+                hint_text: "Felhasználónév"
+                pos_hint: {'center_x': 0.5}
+                size_hint_x: None
+                width: "300dp"
+                icon_left: "account"
+            
+            MDFillRoundFlatIconButton:
+                text: "Regisztráció"
+                icon: "note"
+                font_name: "fonts/Comfortaa-Regular.ttf"
+                pos_hint: {'center_x': 0.5}
+                size_hint: None, None
+                width: "300dp"
+                on_press:
+                    app.sign_up()
+                    root.manager.transition.direction = "left"
 """
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy_lang.kivy_lang import KV
 import pyrebase
+import datetime
 
 firebase_config = {
     'apiKey': "AIzaSyDCCgo6Iq_Xzrfh1tUJhRh8QTw8T1uhtSo",
@@ -25,6 +28,18 @@ class WindowManager(ScreenManager):
     pass
 
 
+class ClickableTextFieldRound(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
+    pass
+
+
+class ClickableTextFieldRoundPasswordAgain(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
+    pass
+
+
 class LoginScreen(Screen):
     pass
 
@@ -41,6 +56,7 @@ class EasyShopping(MDApp):
     # firebase_url = "https://easyshopping-8e66f-default-rtdb.europe-west1.firebasedatabase.app/.json"
     dialog = None
     currently_logged_in_token = None
+    data = None
 
     def build(self):
         self.theme_cls.theme_style = "Light"
@@ -49,20 +65,20 @@ class EasyShopping(MDApp):
 
     def sign_up(self):
         if self.root.get_screen("register").ids.user_email.text is "" or self.root.get_screen(
-                "register").ids.user_password.text is "" or self.root.get_screen(
-            "register").ids.user_password_again.text is "":
+                "register").ids.register_pw.text is "" or self.root.get_screen(
+                "register").ids.register_pw_again.text is "":
             self.open_error_dialog("Az összes mezőt kötelező kitölteni!")
             return
 
-        if self.root.get_screen("register").ids.user_password.text != self.root.get_screen(
-                "register").ids.user_password_again.text:
+        if self.root.get_screen("register").ids.register_pw.text != self.root.get_screen(
+                "register").ids.register_pw_again.text:
             self.open_error_dialog("A megadott jelszavak nem egyeznek!")
             return
         try:
             auth.create_user_with_email_and_password(self.root.get_screen("register").ids.user_email.text,
-                                                     self.root.get_screen("register").ids.user_password.text)
-            # TODO adatbázisba eltárolni a user adatokat
-            self.root.current = "login"
+                                                     self.root.get_screen("register").ids.register_pw.text)
+            self.store_user_data()
+            self.go_to_login_screen()
         except Exception:
             # TODO túl rövid jelszó vagy használt email probléma eldöntésének lekezelése
             self.open_error_dialog("Már létezik ilyen Email cím!")
@@ -70,9 +86,10 @@ class EasyShopping(MDApp):
     def login(self):
         try:
             login = auth.sign_in_with_email_and_password(self.root.get_screen("login").ids.user_email.text,
-                                                         self.root.get_screen("login").ids.user_password.text)
+                                                         self.root.get_screen("login").ids.login_pw.text)
             self.currently_logged_in_token = login["idToken"]
             self.currently_logged_in_token = auth.refresh(login["refreshToken"])
+            self.go_to_home_screen()
             print("nice")
         except Exception:
             self.open_error_dialog("Nem megfelelő felhasználónév vagy jelszó!")
@@ -94,6 +111,23 @@ class EasyShopping(MDApp):
 
     def close_dialog(self, obj):
         self.dialog.dismiss()
+
+    def store_user_data(self):
+        self.data = {"email": self.root.get_screen("register").ids.user_email.text,
+                     "date_of_birth": self.root.get_screen("register").ids.date_of_birth.text,
+                     "username": self.root.get_screen("register").ids.username.text,
+                     "timestamp": str(datetime.datetime.now())
+                     }
+        db.child("users").child(self.root.get_screen("register").ids.username.text).set(self.data)
+
+    def go_to_login_screen(self):
+        self.root.current = "login"
+
+    def go_to_register_screen(self):
+        self.root.current = "register"
+
+    def go_to_home_screen(self):
+        self.root.current = "home"
 
 
 if __name__ == '__main__':
